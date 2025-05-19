4aa62fd631b4968d9e781d351462b79ccaed0be5
Anupam0-0 <anupamchoudhary04@gmail.com>
2025-05-19 08:01:42 +0530

    refactor

---
+++ src/data.js
@@ -0,0 +1,121 @@
+  // --- Data for scenes and previews ---
+export const scenes = [
+    {
+      title: "Haute Couture Nights — Paris",
+      images: [
+        "../assets/img1.webp",
+        "../assets/img2.webp",
+        "../assets/img3.webp",
+        "../assets/img4.webp",
+      ],
+      gridItems: [
+        {
+          img: "assets/img1.webp",
+          caption: "Kai Vega",
+        },
+        {
+          img: "assets/img2.webp",
+          caption: "Riven Juno",
+        },
+        {
+          img: "assets/img3.webp",
+          caption: "Lex Orion",
+        },
+        {
+          img: "assets/img4.webp",
+          caption: "Ash Kairos",
+        },
+        {
+          img: "assets/img5.webp",
+          caption: "Juno Sol",
+        },
+        {
+          img: "assets/img6.webp",
+          caption: "Soren Nyx",
+        },
+        {
+          img: "assets/img7.webp",
+          caption: "Quinn Axon",
+        },
+        {
+          img: "assets/img8.webp",
+          caption: "Zara Voss",
+        },
+        {
+          img: "assets/img9.webp",
+          caption: "Hale B.",
+        },
+        {
+          img: "assets/img10.webp",
+          caption: "Gundra Wex",
+        },
+        {
+          img: "assets/img11.webp",
+          caption: "Extra One",
+        },
+        {
+          img: "assets/img12.webp",
+          caption: "Extra Two",
+        },
+      ],
+    },
+    {
+      title: "Vogue Evolution — New York City",
+      images: [
+        "../assets/img13.webp",
+        "../assets/img14.webp",
+        "../assets/img15.webp",
+        "../assets/img16.webp",
+      ],
+      gridItems: [
+        {
+          img: "assets/img13.webp",
+          caption: "Arlo Quinn",
+        },
+        {
+          img: "assets/img14.webp",
+          caption: "Vera Kline",
+        },
+        {
+          img: "assets/img15.webp",
+          caption: "Juno Vale",
+        },
+        {
+          img: "assets/img16.webp",
+          caption: "Ember Dash",
+        },
+        {
+          img: "assets/img17.webp",
+          caption: "Rylee Voss",
+        },
+        {
+          img: "assets/img18.webp",
+          caption: "Harlow Nova",
+        },
+        {
+          img: "assets/img19.webp",
+          caption: "Blake Lune",
+        },
+        {
+          img: "assets/img22.webp",
+          caption: "Zephyr Kade",
+        },
+        {
+          img: "assets/img21.webp",
+          caption: "Indigo Rae",
+        },
+        {
+          img: "assets/img22.webp",
+          caption: "Kairo Jett",
+        },
+        {
+          img: "assets/img23.webp",
+          caption: "Extra One",
+        },
+        {
+          img: "assets/img24.webp",
+          caption: "Extra Two",
+        },
+      ],
+    },
+  ];
