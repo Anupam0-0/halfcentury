3a654612d4bf8902fe168e9df5986db68ba183d7
Anupam0-0 <anupamchoudhary04@gmail.com>
2025-05-19 09:41:35 +0530

    applying loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
 import React, { useEffect } from "react";
 import Project from "./Project";
+import imagesLoaded from "imagesloaded";
 // import Utopia from './gpt/App'
 
 const App = () => {
+
+
   useEffect(() => {
-    const timer = setTimeout(() => {
+    const preloadImages = (selector = 'img') => {
+      return new Promise((resolve) => {
+        // The imagesLoaded library is used to ensure all images (including backgrounds) are fully loaded.
+        imagesLoaded(document.querySelectorAll(selector), { background: true }, resolve);
+      });
+    };
+
+    preloadImages().then(() => {
       const loading = document.querySelector(".loading");
       if (loading) {
-        loading.classList.remove("loading");
+      loading.classList.remove("loading");
       }
-    }, 2000);
-    return () => clearTimeout(timer);
+    });
   }, []);
 
   
